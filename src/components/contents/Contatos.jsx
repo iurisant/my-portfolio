7d73e50bd925f0b89e832012e78b45c98eb38869
Iuri Santana <iuri.s.goes@gmail.com>
2023-06-27 21:26:34 -0300

    Fix: css formulario footer

--- src/components/contents/Contatos.jsx
+++ src/components/contents/Contatos.jsx
@@ -1,11 +1,11 @@
 export default function Contatos() {
   return (
     <section id="Contatos" className='relative z-10 bg-black text-white flex flex-col items-center justify-center overflow-hidden w-full'>
-      <div className='mx-20'>
+      <div className='mx-20 w-full flex flex-col items-center justify-center'>
         <h2 className='my-20 font-extrabold text-5xl'>Contato</h2>
-        <div className='flex flex-col gap-4 w-full mb-20'>
+        <div className='flex flex-col gap-4 mb-20 w-9/12'>
           <input type="text" id="name" placeholder="Nome"
-            className='bg-transparent border-[0.5px] border-white/50 rounded p-4 wfull'
+            className='bg-transparent border-[0.5px] border-white/50 rounded p-4'
           />
           <input type="mail" id="mail" placeholder="Email"
             className='bg-transparent border-[0.5px] border-white/50 rounded p-4'
