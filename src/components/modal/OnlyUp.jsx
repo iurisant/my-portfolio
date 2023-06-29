7b3c5ff2d1406f13dea8c7e218a7189583d588a7
Iuri Santana <iuri.s.goes@gmail.com>
2023-06-28 22:58:59 -0300

    Css: exibir botão de voltar ao topo somente no desktop

--- src/components/modal/OnlyUp.jsx
+++ src/components/modal/OnlyUp.jsx
@@ -2,7 +2,7 @@ import { ChevronUp } from 'lucide-react'
 
 export default function OnlyUp() {
   return (
-    <a href='#Inicio' className='fixed z-40 bottom-5 right-5 bg-sky-800 w-12 h-12 flex items-center justify-center rounded'>
+    <a href='#Inicio' className='fixed z-40 bottom-5 right-5 bg-sky-800 w-12 h-12 hidden lg:flex items-center justify-center rounded '>
       <ChevronUp color='white' size='28' />
     </a>
   )
