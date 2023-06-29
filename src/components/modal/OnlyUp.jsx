25c640075d05333956d42cbd07064a24002159bf
Iuri Santana <iuri.s.goes@gmail.com>
2023-06-28 23:50:13 -0300

    Add: função para exibir botão de voltar ao topo apenas quando usar o scrol para baixo

--- src/components/modal/OnlyUp.jsx
+++ src/components/modal/OnlyUp.jsx
@@ -1,9 +1,29 @@
-import { ChevronUp } from 'lucide-react'
+import { ChevronUp } from 'lucide-react';
+import { useEffect, useState } from 'react';
 
 export default function OnlyUp() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      setIsVisible(scrollTop > 300);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <a href='#Inicio' className='fixed z-40 bottom-5 right-5 bg-sky-800 w-12 h-12 hidden lg:flex items-center justify-center rounded '>
-      <ChevronUp color='white' size='28' />
-    </a>
-  )
+    <>
+      {isVisible && (
+        <a href="#Inicio" className="fixed z-40 bottom-5 right-5 bg-sky-800 w-12 h-12 hidden lg:flex items-center justify-center rounded">
+          <ChevronUp color="white" size="28" />
+        </a>
+      )}
+    </>
+  );
 }
